36c0d7894257773a55a391be663c6d4f9f967d2e
JuniorLT <Lam2@calcharter.org>
2019-12-27 09:59:03 -0800

    Focused the name element
    Added the javascript script
    Added code that makes cursor appear in the name field

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// TechDegree-Project3
+// Junior Lam Tiang
+
+//selects the name element by the id
+var nameElem = document.getElementById('name');
+
+//focuses the name element so that when the page reloads name is in focus state
+nameElem.focus();
