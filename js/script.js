b9136889800601e27ae71dea4ca15c6245ab251f
JuniorLT <Lam2@calcharter.org>
2019-12-27 21:22:50 -0800

    Job Role Field Updated
    Changed name selection into jQuery
    
    Added conditional to check if other option has been selected
    
    Added function that will keep checking if other option has been selected
    
    Made sure it displays when js is disabled
    
    Rearranged variables

--- js/script.js
+++ js/script.js
@@ -2,7 +2,31 @@
 // Junior Lam Tiang
 
 //selects the name element by the id
-var nameElem = document.getElementById('name');
+var $nameElem = $("#name");
+
+// selects the job role element
+var $jobRole = $("#title");
+
+// selects the other role option
+var $otherRole = $("#other");
 
 //focuses the name element so that when the page reloads name is in focus state
-nameElem.focus();
+$nameElem.focus();
+
+// checks initially if other option is selected
+if ($jobRole.val() == "other"){
+  // if selected show input
+  $otherRole.attr("type", "text");
+}else{
+  $otherRole.attr("type", "hidden");
+}
+
+// keeps checking to see if other option has been selected
+$jobRole.change(function(){
+  if ($jobRole.val() == "other"){
+    // if selected show input
+    $otherRole.attr("type", "text");
+  }else{
+    $otherRole.attr("type", "hidden");
+  }
+});
